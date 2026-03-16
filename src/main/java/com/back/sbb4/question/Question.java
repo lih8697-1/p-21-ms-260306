6f89247d82506459009c1c629a74871d5ffee6ae
lih8697-1 <lih8697@gmail.com>
2026-03-16 17:25:21 +0900

    2-4

---
+++ src/main/java/com/back/sbb4/question/Question.java
@@ -0,0 +1,29 @@
+package com.back.sbb4.question;
+
+import com.back.sbb4.answer.Answer;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 데이터 저장 시 자동으로 1씩 증가
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
