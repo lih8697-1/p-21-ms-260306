beb8ef213aa1a47effc1ee9bf64b4a2f7f41bc3b
lih8697-1 <lih8697@gmail.com>
2026-03-17 23:27:24 +0900

    3-13

---
+++ src/src/main/java/com/back/sbb4/Sbb4Application.java
@@ -0,0 +1,12 @@
+package com.back.sbb4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbb4Application.class, args);
+    }
+}
